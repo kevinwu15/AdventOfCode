b372ef4bd4942c85b796dee0f4219b839aed2bac
Kevin Wu <kevwuhoo15@gmail.com>
2022-12-03 00:31:41 -0500

    completed day 3

---
+++ 2022/day-03/main.py
@@ -0,0 +1,44 @@
+def get_input():
+    f = open("2022/day-03/input.txt", "r")
+    puzzle_input = f.read().strip().split("\n")
+    return puzzle_input
+
+letters = {}
+for i in range(97, 123):
+    letters[chr(i)] = i - 96
+    letters[chr(i - 32)] = i - 70
+
+def part1(puzzle_input, letters):
+    sum = 0
+    for i in puzzle_input:
+        chars = []
+        first = slice(0, len(i) // 2)
+        second = slice(len(i) // 2, len(i))
+        for j in range(len(i) // 2):
+            chars.append(i[first][j])
+        for k in range(len(i) // 2):
+            if i[second][k] in chars:
+                sum += letters.get(i[second][k])
+                break
+    return sum
+                
+def part2(puzzle_input, letters):
+    sum = 0
+    for i in range(0, len(puzzle_input) - 2, 3):
+        layerOne = []
+        layerTwo = []
+        for j in puzzle_input[i]:
+            layerOne.append(j)
+        for k in puzzle_input[i + 1]:
+            if k in layerOne:
+                layerTwo.append(k)
+        for l in puzzle_input[i + 2]:
+            if l in layerTwo:
+                sum += letters.get(l)
+                break
+    return sum
+
+puzzle_input = get_input()
+
+print("Part 1: {}".format(part1(puzzle_input, letters)))
+print("Part 2: {}".format(part2(puzzle_input, letters)))
